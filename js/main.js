464844c4f6ce709a9ba568b812775f7b72c31cfa
Le Hong Quan <quan.le@letsee.io>
2021-02-16 17:11:00 +0900

    Refactor code.

--- js/main.js
+++ js/main.js
@@ -16,30 +16,12 @@ let entity;
  * Reset slides and additional panels.
  */
 function resetSlide() {
-  console.warn(`resetSlide`);
   setTimeout(() => {
-    // add1Active.style.display = 'block';
-    // add2Active.style.display = 'block';
-    // add3Active.style.display = 'block';
 
     add01.style.display = 'none';
     add02.style.display = 'none';
     add03.style.display = 'none';
   }, 10)
-
-  sliderList.forEach(xr => {
-    xr.position._y = 0;
-    xr.opacity = 0;
-
-    /*TweenLite.to(
-        value.element,
-        0,
-        {
-          opacity: 0,
-          display: 'none',
-        },
-    );*/
-  });
 }
 
 /**
@@ -64,49 +46,7 @@ function slide(num) {
       add03.style.display = 'block';
       break;
   }
-
-  /* const ele = sliderList[num];
-  TweenLite.to(
-      ele.position,
-      0.5,
-      {
-        ease: Back.easeOut.config(1.7),
-        _y: -275,
-      });
-
-  TweenLite.to(
-      ele.element,
-      0.5,
-      {
-        opacity: 1,
-        display: 'block',
-      },
-  );*/
-
 }
-
-/*function changeModelAngle() {
-  console.info(`CHANGE MODEL ANGLE`);
-
-  if (TWEEN) TWEEN.removeAll();
-
-  let startValue = {
-    rotationXValue: mesh.rotation.x,
-  };
-  let endValue = {
-    // zoomValue: 5,
-    rotationXValue: Math.PI/180 * 25,
-  };
-
-  let tween = new TWEEN.Tween(startValue).to(endValue, 800);
-
-  tween.onUpdate(() => {
-    mesh.rotation.x = startValue.rotationXValue;
-  });
-  tween.onComplete(() => {});
-
-  tween.start();
-}*/
 
 /**
  * Initialize Swiper elements.
@@ -183,29 +123,9 @@ function initApp() {
 
   addXRElementsIntoEntity();
 
-  /*const add01 = new letsee.DOMRenderable(document.getElementById('additional01'));
-  const add02 = new letsee.DOMRenderable(document.getElementById('additional02'));
-  const add03 = new letsee.DOMRenderable(document.getElementById('additional03'));
-  sliderList = [add01, add02, add03];*/
-
-  setTimeout(function() {
-    /*document.getElementById('add1Active').addEventListener('click', (e) => {
-      resetSlide();
-      slide(0);
-    });
-    document.getElementById('add2Active').addEventListener('click', (e) => {
-      resetSlide();
-      slide(1);
-    });
-    document.getElementById('add3Active').addEventListener('click', (e) => {
-      resetSlide();
-      slide(2);
-    });*/
-
-    document.querySelectorAll('.closeBtn').forEach(val => {
-      val.addEventListener('click', resetSlide);
-    });
-  }, 0);
+  document.querySelectorAll('.closeBtn').forEach(val => {
+    val.addEventListener('click', resetSlide);
+  });
 }
 
 window.onload = () => {
